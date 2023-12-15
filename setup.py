17530cad94b17a6722e1bc4464482b92317bd806
Leonardo Luiz <lcoutinholuiz@gmail.com>
2023-12-15 19:00:22 -0300

    Wrong pip

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import subprocess
 
 def install_requirements():
     try:
-        subprocess.check_call(['pip', 'install', '-r', 'utils/requirements.txt'])
+        subprocess.check_call(['pip3', 'install', '-r', 'utils/requirements.txt'])
         print("Requirements installed successfully.")
     except subprocess.CalledProcessError as e:
         print(f"Error: Failed to install requirements. {e}")
